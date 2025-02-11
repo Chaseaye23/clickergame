67ce2db3153f4f5b94a80355a31e47ff019a01f0
[Chase DeMey] <[manutdchase@gmail.com]>
2025-02-10 23:09:43 -0500

    1

--- js/main.js
+++ js/main.js
@@ -8,6 +8,23 @@
 
 
   /*----- event listeners -----*/
- /* */
+ 
 
   /*----- functions -----*/
+let canvas = document.getElementById('myCanvas');
+let ctx = canvas.getContext('2d');
+
+let player = {
+    x: 100,
+    y: 100,
+    width: 50,
+    height: 50
+};
+
+function update() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillRect(player.x, player.y, player.width, player.height); 
+    requestAnimationFrame(update); 
+}
+
+update(); 
